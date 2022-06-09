a8e4b6012554f12cc619a9db9144673bb2e2ac03
leoville <sebastien.leoville@gmail.com>
2022-06-09 11:12:38 +0200

    first commit

---
+++ components/index.js
@@ -0,0 +1,9 @@
+export { default as HomeHeader } from './HomeHeader'
+export { default as NFTCard } from './NFTCard'
+export { default as FocusedStatusBar } from './FocusedStatusBar'
+export { default as DetailsDesc } from './DetailsDesc'
+export { default as DetailsBid } from './DetailsBid'
+export { SubInfo } from './SubInfo'
+
+
+export { CircleButton, RectButton } from './Button'
